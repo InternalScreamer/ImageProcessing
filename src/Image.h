415a3f9c5c654ffcff59df7cedbf8ffa8385e1cd
InternalScreamer <nchad95@gmail.com>
2020-07-17 02:10:51 -0700

    Adding header changes

--- src/Image.h
+++ src/Image.h
@@ -3,24 +3,37 @@
 #ifndef IMAGE_H_
 #define IMAGE_H_
 #include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
 
+using namespace std;
 class image
 {
     public:
-    image();
+    image(string file, unsigned int id);
     ~image();
     void initialize();
     //Make parse a separate function to future threadings
-    virtual void parse();
+    void parse();
 
-    std::vector<uint8_t> red;
-    std::vector<uint8_t> green;
-    std::vector<uint8_t> blue;
-
+    //variables
+    vector<uint8_t> red;
+    vector<uint8_t> green;
+    vector<uint8_t> blue;
+    vector<uint8_t> alpha;
+    vector<uint8_t> gray;
+    string filename;
+    
+    uint32_t id;
+    uint32_t width;
+    uint32_t height;
+    
+    uint8_t mode;
+    
     private:
-    virtual void extractHeader();
-    virtual void verify();
-
+    void extractHeader();
+    vector<uint8_t> populateFromFile(int offset, int size);
 };
 
 #endif
