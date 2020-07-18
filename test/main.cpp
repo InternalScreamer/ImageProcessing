cedffeb12f5ffcc34455d1215a231015ffbe9e3d
InternalScreamer <nchad95@gmail.com>
2020-07-18 01:17:55 -0700

    Added CMake build. Need to update all my documentation.

--- test/main.cpp
+++ test/main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main()
 {
-    string file = "ReformattedImages/testImage5";
+    string file = "test/ReformattedImages/testImage5";
     image* test_image = new image(file, 0);
     cout<< test_image->height<<endl;
     cout<< test_image->width<<endl;
     cout<< static_cast<int>(test_image->mode)<<endl;
     test_image->parse();
     cout<<test_image->red.size()<<endl;
+    delete test_image;
     return 0;
 }
